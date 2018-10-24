4be4f28947f2ccb793cee29f42c28693656af3da
whitfordr <whitfordR@berea.edu>
2018-10-24 11:45:53 -0400

    Edited places.txt
    added new entries to the tuple, but theyre not working yet. Need to change a couple data types to floats

--- t10_places.py
+++ t10_places.py
@@ -1,6 +1,6 @@
 ######################################################################
-# Author: Emily Lovell & Scott Heggen      TODO: Change this to your names
-# Username: lovelle & heggens             TODO: Change this to your usernames
+# Author: Rebekah Whitford & Jacob Hill
+# Username: WhitfordR & HillJ
 #
 # Assignment: T10: Oh, the Places You'll Go!
 #
@@ -96,10 +96,14 @@ def extract_place(file_content):
     # TODO   the line starts with a #; if it does, ignore that line.
 
     name = file_content.readline().strip("\n")
+    location = file_content.readline().strip("\n")
+    latitude = file_content.readline().strip("\n")
+    longitude = file_content.readline().strip("\n")
+    user_color = file_content.readline().strip("\n")
 
     # FIXME Construct a tuple with all five values in the correct order. Don't forget types, and tuples are immutable!
     # Example: place_tuple = ("Scott's example", "Somewhere special", 41, -10, "black")
-    place_tuple = (name, )      # Finish assembling the tuple!
+    place_tuple = (name, location, latitude, longitude, user_color)      # Finish assembling the tuple!
     return place_tuple
 
 
